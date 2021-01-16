9094e816604b5eb1a65a03bca120093870048c2a
Juaoie <uaoie@qq.com>
2021-01-16 22:28:48 +0800

    不支持使用data属性

--- components/u-cache/u-cache.vue
+++ components/u-cache/u-cache.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <slot :data="data" :cache="cache"></slot>
+    <slot :extend="extend" :cache="cache"></slot>
   </div>
 </template>
 
@@ -11,7 +11,7 @@ export default {
   props: {
     cacheId: String,
     cacheName: String,
-    data: Object,
+    extend: Object,
   },
   data() {
     return { cache: {} };
